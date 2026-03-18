ded02535c7bd7af93cd760d89c2bb0ddf82c72fd
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-03-18 16:49:38 +0200

    tsIndex

--- 01Basics/index.ts
+++ 01Basics/index.ts
@@ -9,7 +9,25 @@ type User = {
     }
 }
 
-
+//soovin näidata ainult name ja age, aga võetakse kogu objekti sisu
+//kuna kasutatakse User type
 function renderUserDetails(user: User) {
     console.log(user.name, user.age)
 }
+
+const user: User = {
+    id: "ads",
+    name: "Kyle",
+    age: 123,
+    address: {
+        street: "sdf",
+        city: "London"
+    }
+}
+
+renderUserDetails(user)
+
+//NB! selleks, et näha konsoolis tulemust
+//1. tsc tsFailiNimi.ts
+//2. genereerib js faili
+//3. node index.js käsklus tuleb sisestada
