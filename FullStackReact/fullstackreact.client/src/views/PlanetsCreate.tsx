e510d15252dd5f80f503b8dae4a05ad4c14a5993
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-05-13 16:47:01 +0300

    PlanetDetails

--- FullStackReact/fullstackreact.client/src/views/PlanetsCreate.tsx
+++ FullStackReact/fullstackreact.client/src/views/PlanetsCreate.tsx
@@ -14,7 +14,6 @@ export default function PlanetsCreate() {
     const navigate = useNavigate();
 
     const [form, setForm] = useState<FormState>({
-        //id: "",
         name: "",
         description: "",
         type: "",
